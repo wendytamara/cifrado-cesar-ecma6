1c850fe40c0aad22188957e924989b22fba58f58
wendytamara <anacarlavegam@gmail.com>
2018-01-26 06:12:37 -0500

    añadiendo campo de desplazamiento

--- js/app.js
+++ js/app.js
@@ -8,9 +8,10 @@ const regex = /^[a-zA-Z]*$/;
 
 // funcion para cifrar
 btnCipher.on('click', () => {
-
-  const wordsToEncrypt = $('#input-cesar').val();
-  const container = $('#content-cipher');
+debugger
+  let wordsToEncrypt = $('#input-cesar').val();
+  let container = $('#content-cipher');
+  let desplazamiento = parseInt($('#desplazamiento').val());
 
   if (wordsToEncrypt && regex.test(wordsToEncrypt) ) {
     let chartValue = 0;
@@ -19,12 +20,12 @@ btnCipher.on('click', () => {
        for (var i=0; i<wordsToEncrypt.length; i++){
          chartValue = wordsToEncrypt.charCodeAt(i);
          if (chartValue>=65 && chartValue<=90){
-             chartValue = (chartValue - 65 + 33)%26+65;
+             chartValue = (chartValue - 65 + desplazamiento)%26+65;
              chartValue = String.fromCharCode(chartValue);
              cipherWord = cipherWord + chartValue
          }
          else if(chartValue>=97 && chartValue<=122){
-             chartValue = (chartValue - 97 + 33)%26+97;
+             chartValue = (chartValue - 97 + desplazamiento)%26+97;
              chartValue = String.fromCharCode(chartValue);
              cipherWord = cipherWord + chartValue
          }
@@ -48,6 +49,7 @@ btnDecipher.on('click', () => {
 
   const wordsToEncryptDecipher = $('#input-cesar').val();
   const container = $('#content-cipher');
+  let desplazamiento = parseInt($('#desplazamiento').val());
 
   if (wordsToEncryptDecipher && regex.test(wordsToEncryptDecipher)) {
     let chartValue2 = 0;
@@ -56,12 +58,12 @@ btnDecipher.on('click', () => {
        for (var i=0; i<wordsToEncryptDecipher.length ; i++){
          chartValue2 = wordsToEncryptDecipher.charCodeAt(i);
          if (chartValue2>=65 && chartValue2<=90){
-             chartValue2 = (chartValue2 + 65 - 33)%26+65;
+             chartValue2 = (chartValue2 + 65 - desplazamiento)%26+65;
              chartValue2 = String.fromCharCode(chartValue2);
              deCipherWord = deCipherWord+ chartValue2
          }
          else if(chartValue2>=97 && chartValue2<=122){
-             chartValue2 = (chartValue2 + 97 - 45)%26+97;
+             chartValue2 = (chartValue2 + 97 - 45 )%26+97;
              chartValue2 = String.fromCharCode(chartValue2);
              deCipherWord = deCipherWord+ chartValue2
          }
